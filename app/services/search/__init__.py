0864631aa7c160df221ab981b638d52d98a76b35
turnerburchard <tburchard4@gmail.com>
2025-04-09 14:20:53 -0600

    rename

--- app/services/search/__init__.py
+++ app/services/search/__init__.py
@@ -1,4 +1,4 @@
-from .service import SearchService
+from .search import SearchService
 from .config import SearchConfig, default_config
 
 __all__ = ['SearchService', 'SearchConfig', 'default_config'] 
